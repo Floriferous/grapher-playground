b35f6c390b1c035fac1878b430842c095cd1de59
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-22 15:56:30 +0200

    Add a few more tests to check links are working

--- imports/api/subLinks.test.js
+++ imports/api/subLinks.test.js
@@ -6,37 +6,65 @@ import Links from './links';
 import SubLinks from './subLinks';
 import './grapherLinks';
 
-describe('remove', () => {
+describe('SubLinks', () => {
   beforeEach(() => {
     Links.remove({});
     SubLinks.remove({});
   });
 
-  it('does not remove the parent object', () => {
-    const subLinkId = SubLinks.insert({});
-    const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
-    let link = Links.findOne(linkId);
+  describe('queries', () => {
+    it('links are working', () => {
+      const subLinkId = SubLinks.insert({});
+      const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
 
-    expect(Links.find({}).count()).to.equal(1);
-    expect(SubLinks.find({}).count()).to.equal(1);
+      const subLink = SubLinks.createQuery({
+        $filters: { _id: subLinkId },
+        links: { _id: 1 }
+      }).fetchOne();
 
-    SubLinks.remove(subLinkId);
+      expect(subLink.links[0]._id).to.equal(linkId);
+    });
 
-    expect(Links.find({}).count()).to.equal(1);
-    expect(SubLinks.find({}).count()).to.equal(0);
+    it('links are working', () => {
+      const subLinkId = SubLinks.insert({});
+      const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
+
+      const link = Links.createQuery({
+        $filters: { _id: linkId },
+        subLinks: { _id: 1 }
+      }).fetchOne();
+
+      expect(link.subLinks[0]._id).to.equal(subLinkId);
+    });
   });
 
-  it('should delete the link id', () => {
-    const subLinkId = SubLinks.insert({});
-    const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
-    let link = Links.findOne(linkId);
+  describe('remove', () => {
+    it('does not remove the parent object when removed', () => {
+      const subLinkId = SubLinks.insert({});
+      const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
+      let link = Links.findOne(linkId);
 
-    expect(link.subLinkIds).to.deep.equal([{ _id: subLinkId }]);
+      expect(Links.find({}).count()).to.equal(1);
+      expect(SubLinks.find({}).count()).to.equal(1);
 
-    SubLinks.remove(subLinkId);
+      SubLinks.remove(subLinkId);
 
-    link = Links.findOne(linkId);
+      expect(Links.find({}).count()).to.equal(1);
+      expect(SubLinks.find({}).count()).to.equal(0);
+    });
 
-    expect(link.subLinkIds).to.deep.equal([]);
+    it('should delete the link id', () => {
+      const subLinkId = SubLinks.insert({});
+      const linkId = Links.insert({ subLinkIds: [{ _id: subLinkId }] });
+      let link = Links.findOne(linkId);
+
+      expect(link.subLinkIds).to.deep.equal([{ _id: subLinkId }]);
+
+      SubLinks.remove(subLinkId);
+
+      link = Links.findOne(linkId);
+
+      expect(link.subLinkIds).to.deep.equal([]);
+    });
   });
 });
